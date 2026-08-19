ec25888bbf6d6c848cae82b80f7dc04154f41bb4
Samim29 <samimalisk000@gmail.com>
2026-08-19 16:00:04 +0530

    fixed Login import

--- client/src/app.routes.jsx
+++ client/src/app.routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Register from "./features/auth/pages/Register";
-import Login from "./features/auth/pages/login";
+import Login from "./features/auth/pages/Login";
 import PgForm from "./features/pg/pages/PgForm";
 import OwnerRoute from "./features/auth/components/OwnerRoute";
 import Home from "./features/pg/pages/Home";
